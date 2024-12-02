8b50d172dc3d3a8561743b700ec5e97670ffba5d
bhupesh-roushan <roushan.bhupesh@gmail.com>
2024-12-02 14:53:15 +0530

    addes controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(
     credentials: true,
   })
 );
- 
+
 app.use(
   express.json({
     limit: "16kb",
@@ -20,5 +20,17 @@ app.use(
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes Declaration
+app.use("/api/v1/users",userRouter)
+
+
+
 
 export { app };
